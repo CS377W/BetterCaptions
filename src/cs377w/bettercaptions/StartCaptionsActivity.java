6f360ec78c066e4487bfedafe4898f4861f4f115
Brie Bunge <briebunge@gmail.com>
2014-05-05 17:09:44 -0700

    lyric sync fix

--- src/cs377w/bettercaptions/StartCaptionsActivity.java
+++ src/cs377w/bettercaptions/StartCaptionsActivity.java
@@ -6,13 +6,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.widget.TextView;
 
 import com.firebase.client.*;
@@ -26,24 +28,37 @@ public class StartCaptionsActivity extends Activity {
 		public String text;
 	}
 
-	private SubtitleFile subtitleFile;
-	private String currentlyCaptioning;
+	private SubtitleFile subtitleFile = null;
+	private String currentlyCaptioning = null;
 
-	private String currentSongName;
-	private List<LyricLine> currentLyrics;
+	private String currentSongName = null;
+	private List<LyricLine> currentLyrics = null;
+	private Double currentSongElapsedTime = null;
+	private Timer timer = null;
+	private String lyricText = "";
+
+	private TextView tv;
+	
+	public Handler mHandler = new Handler() {
+	    public void handleMessage(Message msg) {
+	    	if (tv == null)
+	    		return;
+	        tv.setText(lyricText);
+	    }
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_captions);
 
-		subtitleFile = null;
-		currentlyCaptioning = null;
-		currentSongName = null;
-		currentLyrics = null;
+		tv = (TextView) findViewById(R.id.caption_content);
 
-		final TextView tv = (TextView) findViewById(R.id.caption_content);
+		addMovieListeners();
+		addMusicListeners();
+	}
 
+	private void addMovieListeners() {
 		Firebase nameRef = new Firebase(
 				"https://fiery-fire-3139.firebaseio.com/current_name");
 		nameRef.addValueEventListener(new ValueEventListener() {
@@ -118,10 +133,12 @@ public class StartCaptionsActivity extends Activity {
 			public void onCancelled(FirebaseError error) {
 			}
 		});
+	}
 
-		Firebase songRef = new Firebase(
-				"https://fiery-fire-3139.firebaseio.com/current_song");
-		songRef.addValueEventListener(new ValueEventListener() {
+	private void addMusicListeners() {
+		Firebase songTitleRef = new Firebase(
+				"https://fiery-fire-3139.firebaseio.com/current_song/title");
+		songTitleRef.addValueEventListener(new ValueEventListener() {
 
 			@Override
 			public void onDataChange(DataSnapshot snap) {
@@ -131,55 +148,11 @@ public class StartCaptionsActivity extends Activity {
 				if (value == null)
 					return;
 
-				String title = (String) ((Map) value).get("title");
+				String title = (String) value;
 
 				if (currentSongName != title) {
-					currentLyrics = new ArrayList<>();
-
-					try {
-						InputStream is = getAssets().open(title + ".lrc");
-						String str;
-						BufferedReader br = new BufferedReader(
-								new InputStreamReader(is));
-						String line = null;
-
-						Pattern pattern = Pattern
-								.compile("\\[(\\d+):(\\d+)\\.(\\d+)\\] (.*)");
-						while ((line = br.readLine()) != null) {
-							Matcher m = pattern.matcher(line);
-							if (m.find()) {
-								int count = m.groupCount();
-
-								int minute = Integer.parseInt(m.group(1));
-								double seconds = Double.parseDouble(m.group(2) + "." + m.group(3));
-								seconds += 60 * minute;
-								String lyric = m.group(4);
-
-								LyricLine lyricLine = new LyricLine();
-								lyricLine.text = lyric;
-								lyricLine.seconds = seconds;
-
-								currentLyrics.add(lyricLine);
-							}
-						}
-
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
 					currentSongName = title;
-				}
-
-				Double currentTime = (Double) ((Map) value).get("current_time");
-				
-				LyricLine prevLyric = null;
-				for (LyricLine lyric : currentLyrics) {
-					if (lyric.seconds > currentTime) break;
-					prevLyric = lyric;
-				}
-				if (prevLyric != null) {
-					tv.setText(prevLyric.text);
+					parseLRC();
 				}
 			}
 
@@ -188,5 +161,122 @@ public class StartCaptionsActivity extends Activity {
 			}
 		});
 
+		Firebase songTimeRef = new Firebase(
+				"https://fiery-fire-3139.firebaseio.com/current_song/current_time");
+		songTimeRef.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot snapshot) {
+				if (snapshot == null || snapshot.getValue() == null)
+					return;
+
+				if (currentLyrics == null)
+					return;
+
+				Double currentTime = (Double) snapshot.getValue();
+				System.out.println("MYLOG: currentTime: " + currentTime);
+				currentSongElapsedTime = currentTime;
+				scheduleLyrics();
+			}
+
+			@Override
+			public void onCancelled(FirebaseError error) {
+				System.err.println("Listener was cancelled");
+			}
+		});
 	}
+
+	private void parseLRC() {
+		currentLyrics = new ArrayList<>();
+
+		try {
+			InputStream is = getAssets().open(currentSongName + ".lrc");
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			String line = null;
+
+			Pattern pattern = Pattern
+					.compile("\\[(\\d+):(\\d+)\\.(\\d+)\\] (.*)");
+			while ((line = br.readLine()) != null) {
+				Matcher m = pattern.matcher(line);
+				if (m.find()) {
+					int minute = Integer.parseInt(m.group(1));
+					double seconds = Double.parseDouble(m.group(2) + "."
+							+ m.group(3));
+					seconds += 60 * minute;
+					String lyric = m.group(4);
+
+					LyricLine lyricLine = new LyricLine();
+					lyricLine.text = lyric;
+					lyricLine.seconds = seconds;
+
+					currentLyrics.add(lyricLine);
+				}
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private class LyricTimerTask extends TimerTask {
+		private int index = 0;
+
+		public LyricTimerTask(int index) {
+			this.index = index;
+		}
+
+		@Override
+		public void run() {
+			if (currentLyrics == null || currentLyrics.size() < index)
+				return;
+
+			LyricLine lyric = currentLyrics.get(index);
+			currentSongElapsedTime = lyric.seconds;
+			lyricText = lyric.text;
+			mHandler.obtainMessage().sendToTarget();
+		}
+	}
+	
+	private final long LYRIC_SHOW_DELTA = 500;
+	
+	private void scheduleLyrics() {
+		if (timer != null)
+			timer.cancel();
+		timer = new Timer();
+		
+		for (int i = 0; i < currentLyrics.size(); i++) {
+			LyricLine lyric = currentLyrics.get(i);
+			if (lyric.seconds < currentSongElapsedTime)
+				continue;
+			
+			LyricTimerTask task = new LyricTimerTask(i);
+			long milliDelta = (long) (lyric.seconds * 1000 - currentSongElapsedTime * 1000) - LYRIC_SHOW_DELTA;
+			if (milliDelta < 0)
+				continue;
+			timer.schedule(task, milliDelta);
+		}
+	}
+
+//	private void scheduleLyrics() {
+//		int indexOfCurLyric = -1;
+//		for (int i = 0; i < currentLyrics.size(); i++) {
+//			LyricLine lyric = currentLyrics.get(i);
+//			if (lyric.seconds > currentSongElapsedTime)
+//				break;
+//			indexOfCurLyric = i;
+//		}
+//		if (indexOfCurLyric == -1)
+//			return;
+//		
+//		LyricLine lyric = currentLyrics.get(indexOfCurLyric);
+//		lyricText = lyric.text;
+//		mHandler.obtainMessage().sendToTarget();
+//
+//		if (timer != null)
+//			timer.cancel();
+//		timer = new Timer();
+//		
+//		LyricTimerTask firstTask = new LyricTimerTask(indexOfCurLyric);
+//		timer.schedule(firstTask, 0);
+//	}
 }
